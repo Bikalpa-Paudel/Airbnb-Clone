5ad55a1fb7a591e101c07b37d9d65210d98ab599
Bikalpa <bikalpapaudel53@gmail.com>
2022-12-25 12:03:36 +0545

    final commit

--- src/data.js
+++ src/data.js
@@ -9,21 +9,21 @@ const data =[
         
     },
     {
-        id : 1,
-        image : "image1.png",
-        title: "Life lessons with Katie Zaferes",
+        id : 2,
+        image : "./image2.png",
+        title: "Learn wedding photography",
         rating: "5.0",
-        ratingNum: 6,
-        price: 136
+        ratingNum: 30,
+        price: 125
         
     },
     {
-        id : 1,
-        image : "image1.png",
-        title: "Life lessons with Katie Zaferes",
-        rating: "5.0",
-        ratingNum: 6,
-        price: 136
+        id : 3,
+        image : "./image3.png",
+        title: "Group Mountain Biking",
+        rating: "4.8",
+        ratingNum: 2,
+        price: 50
         
     }
     ]
